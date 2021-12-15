441b3fedc3247d7d50c189ed6c1baaa98e813300
Diego Isaac Herrera Ledezma <dhlisaac@gmail.com>
2021-12-14 18:39:00 -0600

    pre npm publish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-undef */
+import {
+  CardActions,
+  CardContent,
+  CardActionArea,
+  Card,
+  Typography,
+  Box,
+  Button,
+  CardMedia,
+  Grid,
+} from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+export default function ListaServicios(servicios) {
+  const tamanoh = 300;
+
+  const getServicesDisplay = () => {
+    let tarjetas = [];
+    servicios.forEach((servicio) => {
+      tarjetas.push(
+        <Card
+          sx={{
+            display: "inline-block",
+            m: 1,
+            width: 400,
+            border: "1p #0CA8C7",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <CardContent>
+              <Grid container>
+                <Grid item xs={12}>
+                  <Grid item xs={12}>
+                    <CardMedia
+                      component="img"
+                      sx={{ display: "inline-block", width: 40 }}
+                      src={servicio.imagenlink}
+                      alt="prueba"
+                    />
+                  </Grid>
+                  <Typography variant="h6">{servicio.titulo}</Typography>
+                  <Typography component="div">
+                Contenido: {servicio.contenido}
+              </Typography>
+                </Grid>
+              </Grid>
+              <Grid item xs={12}>
+                <CardActionArea
+                  component="callToAction"
+                  href={servicio.botonlink}
+                  onClick={() =>
+                    console.log(
+                      "CardActionArea clicked in + " + servicio.titulo
+                    )
+                  }
+                >
+                  <Button variant="outlined">Visitar</Button>
+                </CardActionArea>
+              </Grid>
+            </CardContent>
+          </Box>
+        </Card>
+      );
+    });
+    return tarjetas;
+  };
+
+  return (
+    <div>
+      <Box sx={{ p: 2 }}>{getServicesDisplay()}</Box>
     </div>
   );
 }
-
-export default App;
