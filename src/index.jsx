9570bd84f93c30a3cc09f626f71dd20e993001b7
Diego Isaac Herrera Ledezma <dhlisaac@gmail.com>
2021-12-21 10:16:26 -0600

    npm fix

---
+++ src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ListaServicios from './App';
+
+const listaServicios = [
+  {
+    titulo: "MUI",
+    contenido: "Lorem ipsum",
+    botonlink: "https://mui.com/",
+    imagenlink: "nolink",
+  },
+  {
+    titulo: "YouTube",
+    contenido: "Lorem ipsum",
+    botonlink: "https://www.youtube.com/",
+    imagenlink: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg",
+  },
+  {
+    titulo: "Twitch",
+    contenido: "Lorem ipsum",
+    botonlink: "https://www.twitch.tv/",
+    imagenlink: "nolink",
+  },
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ListaServicios servicios={listaServicios}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
